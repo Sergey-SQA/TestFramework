90dca27592de57bfc1e348ea9202b80f7755c2a5
Sergey-SQA <82242346+Sergey-SQA@users.noreply.github.com>
2021-07-18 22:07:42 -0600

    Add project files.

---
+++ TestFramework_0/Resources/Hooks.cs
@@ -0,0 +1,29 @@
+﻿namespace TestFramework
+{
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using System;
+
+    public class Hooks
+    {
+        public IWebDriver Driver { get; private set; }
+
+        [SetUp]
+        public void SetUp()//must be non-static
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Cookies.DeleteAllCookies();
+            Driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]//must be non-static
+        public void TearDown()
+        {
+            Driver.Manage().Cookies.DeleteAllCookies();
+            Driver.Close();
+        }
+
+     
+    }       
+}
